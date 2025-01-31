ac89e72c6777b763419999de8efbf88f7478f858
origemjhanpoll <origemjhanpoll@gmail.com>
2025-01-31 18:49:41 -0300

    removing contrains

--- lib/features/portfolio/presentation/widgets/home_widget.dart
+++ lib/features/portfolio/presentation/widgets/home_widget.dart
@@ -17,96 +17,91 @@ class HomeWidget extends StatelessWidget {
     final localization = AppLocalizations.of(context)!;
     final urlLauncherUtil = UrlLauncherUtil();
 
-    return SizedBox(
-      height: size.height - kToolbarHeight,
-      child: Flex(
-        direction: isScreenMedium ? Axis.horizontal : Axis.vertical,
-        mainAxisSize: isScreenMedium ? MainAxisSize.max : MainAxisSize.min,
-        verticalDirection: VerticalDirection.up,
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Flexible(
-            child: Padding(
-              padding: EdgeInsets.all(SpacingSize.getPadding(size.width)),
-              child: Column(
-                mainAxisAlignment: isScreenMedium
-                    ? MainAxisAlignment.center
-                    : MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SelectableText(localization.hello,
-                      style: theme.textTheme.displayMedium!.copyWith(
-                          fontWeight: FontWeight.bold,
-                          fontSize: !isScreenMedium ? 28.0 : null)),
-                  SelectableText(
-                    localization.occupation,
-                    style: theme.textTheme.headlineLarge!
-                        .copyWith(fontSize: !isScreenMedium ? 20.0 : null),
+    return Flex(
+      direction: isScreenMedium ? Axis.horizontal : Axis.vertical,
+      mainAxisSize: isScreenMedium ? MainAxisSize.max : MainAxisSize.min,
+      verticalDirection: VerticalDirection.up,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Flexible(
+          child: Padding(
+            padding: EdgeInsets.all(SpacingSize.getPadding(size.width)),
+            child: Column(
+              mainAxisAlignment: isScreenMedium
+                  ? MainAxisAlignment.center
+                  : MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SelectableText(localization.hello,
+                    style: theme.textTheme.displayMedium!.copyWith(
+                        fontWeight: FontWeight.bold,
+                        fontSize: !isScreenMedium ? 28.0 : null)),
+                SelectableText(
+                  localization.occupation,
+                  style: theme.textTheme.headlineLarge!
+                      .copyWith(fontSize: !isScreenMedium ? 20.0 : null),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: SpacingSize.large),
+                  child: SelectableText(
+                    localization.introduction,
+                    style: theme.textTheme.bodyLarge!
+                        .copyWith(overflow: TextOverflow.ellipsis),
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: SpacingSize.large),
-                    child: SelectableText(
-                      localization.introduction,
-                      style: theme.textTheme.bodyLarge!
-                          .copyWith(overflow: TextOverflow.ellipsis),
+                ),
+                Wrap(
+                  spacing: SpacingSize.small,
+                  runSpacing: SpacingSize.small,
+                  children: [
+                    FilledButton.icon(
+                      onPressed: () =>
+                          urlLauncherUtil.launchURL(UrlUtils.resume),
+                      icon: Icon(Icons.attach_file),
+                      style: ButtonStyle(visualDensity: VisualDensity.standard),
+                      label: Text(
+                        localization.myresume,
+                        softWrap: true,
+                      ),
                     ),
-                  ),
-                  Wrap(
-                    spacing: SpacingSize.small,
-                    runSpacing: SpacingSize.small,
-                    children: [
-                      FilledButton.icon(
-                        onPressed: () =>
-                            urlLauncherUtil.launchURL(UrlUtils.resume),
-                        icon: Icon(Icons.attach_file),
-                        style:
-                            ButtonStyle(visualDensity: VisualDensity.standard),
-                        label: Text(
-                          localization.myresume,
-                          softWrap: true,
-                        ),
+                    OutlinedButton.icon(
+                      onPressed: () =>
+                          urlLauncherUtil.launchURL(UrlUtils.whatsapp),
+                      icon: SvgPicture.asset(
+                        'assets/svg/icon_whatsapp.svg',
+                        width: 20.0,
+                        colorFilter: ColorFilter.mode(
+                            theme.colorScheme.secondary, BlendMode.srcIn),
                       ),
-                      OutlinedButton.icon(
-                        onPressed: () =>
-                            urlLauncherUtil.launchURL(UrlUtils.whatsapp),
-                        icon: SvgPicture.asset(
-                          'assets/svg/icon_whatsapp.svg',
-                          width: 20.0,
-                          colorFilter: ColorFilter.mode(
-                              theme.colorScheme.secondary, BlendMode.srcIn),
-                        ),
-                        style:
-                            ButtonStyle(visualDensity: VisualDensity.standard),
-                        label: Text(
-                          overflow: TextOverflow.clip,
-                          localization.getInTouch,
-                          style: theme.textTheme.bodyMedium,
-                          softWrap: true,
-                        ),
+                      style: ButtonStyle(visualDensity: VisualDensity.standard),
+                      label: Text(
+                        overflow: TextOverflow.clip,
+                        localization.getInTouch,
+                        style: theme.textTheme.bodyMedium,
+                        softWrap: true,
                       ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-          Flexible(
-            child: Padding(
-              padding: EdgeInsets.all(
-                  SpacingSize.getPadding(size.width) * (size.width * 0.002)),
-              child: Center(
-                child: ClipOval(
-                  child: Image.asset(
-                    'assets/images/origemjhanpoll.png',
-                    fit: BoxFit.cover,
-                  ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(
+                SpacingSize.getPadding(size.width) * (size.width * 0.002)),
+            child: Center(
+              child: ClipOval(
+                child: Image.asset(
+                  'assets/images/origemjhanpoll.png',
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
