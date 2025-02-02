a8b1b7f43df59229ce0e98b01abc3d14db1e871f
origemjhanpoll <origemjhanpoll@gmail.com>
2025-02-02 18:06:23 -0300

    changing url

--- lib/core/constants/url_utils.dart
+++ lib/core/constants/url_utils.dart
@@ -6,7 +6,7 @@ class UrlUtils {
       'https://github.com/origemjhanpoll?tab=repositories';
   static const email = 'origemjhanpoll@gmail.com';
   static const resume =
-      'https://drive.google.com/file/d/1OemAUp-y0MeplfcQHuMBd6ovddnjeWtP/view?usp=sharing';
+      'https://github.com/origemjhanpoll/origemjhanpoll.github.io/raw/refs/heads/main/assets/resume/Jean%20Paul%20-%20PTBR.pdf';
   static const whatsapp =
       'https://api.whatsapp.com/send/?phone=5598991391108&text&type=phone_number&app_absent=0';
 }
