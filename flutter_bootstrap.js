308dbc9a85871b8cbb825d7983532c121e7ad2ed
origemjhanpoll <origemjhanpoll@users.noreply.github.com>
2025-06-25 02:09:56 +0000

    deploy: 19d0dd23796fdac546b2baa748181ebb92fd9624

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1453697244"
+    serviceWorkerVersion: "2121654422"
   }
 });
